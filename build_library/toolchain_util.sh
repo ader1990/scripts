26fb93c2a5dab2ce3ef51c3a9a0ffa7b31a6d3dd
Adrian Vladu <avladu@cloudbasesolutions.com>
2024-12-22 23:43:14 +0000

    rust: fix rust check for install

--- build_library/toolchain_util.sh
+++ build_library/toolchain_util.sh
@@ -481,7 +481,7 @@ install_cross_rust() {
     [[
        -d /usr/lib/rustlib/x86_64-unknown-linux-gnu &&
        -d /usr/lib/rustlib/aarch64-unknown-linux-gnu &&
-       -d /usr/lib/rustlib/riscv64-unknown-linux-gnu
+       -d /usr/lib/rustlib/riscv64gc-unknown-linux-gnu
     ]] && return
 
     echo "Rebuilding dev-lang/rust with updated cross targets."
